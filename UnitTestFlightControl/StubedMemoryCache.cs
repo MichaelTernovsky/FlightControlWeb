fb0d8c2050406208022ca1d75c7c9bdfc82b5e55
Michael Ternovsky <michael.ternovsky@live.biu.ac.il>
2020-05-31 17:36:05 +0300

    Revert "update the unit tests"
    This reverts commit 638eff8567ec2f11f3504bc311196871220f02e2.

---
+++ UnitTestFlightControl/StubedMemoryCache.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestFlightControl
+{
+    class StubedMemoryCache : IMemoryCache
+    {
+        public ICacheEntry CreateEntry(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(object key, out object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Get(this IMemoryCache cache, object key)
+        {
+
+        }
+
+        public static TItem Set<TItem>(this IMemoryCache cache, object key, TItem value)
+        {
+
+        }
+    }
+}
